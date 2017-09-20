b97c0a5d0fffd22820d0b53753258416ae4056f0
David Millman <dave@cs.unc.edu>
2017-09-19 19:39:25 -0600

    Add interop demo for passing integers

---
+++ interop/c_wrapper.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void to_int_(int *int_ptr) {
+    int i = *int_ptr;
+    printf("i in c = %d\n", i);
+}
---
+++ interop/driver.f90
@@ -0,0 +1,5 @@
+program driver
+    integer :: i=7
+    print *, "i in fortran = ",i
+    call to_int(i)
+end program
