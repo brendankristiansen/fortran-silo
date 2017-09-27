c72c1178dfa4a03206d4c64a7228f7e6496b38d5
Brendan Kristiansen <brendankristiansen@icloud.com>
2017-09-27 16:05:55 -0600

    Dave made this compile

--- interop/c_wrapper.c
+++ interop/c_wrapper.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include<silo.h>
 
-int main(void){
-	print("C Wrapper!\n");
-	return 0;
-}
+//int main(void){
+//	printf("C Wrapper!\n");
+//	return 0;
+//}
 
 void to_int_(int *int_ptr) {
     int i = *int_ptr;
     printf("i in c = %d\n", i);
 }
 
-void add_int_(int *ptr_a, *ptr_b, *ptr_c){
+void add_int_(int *ptr_a, int *ptr_b, int *ptr_c){
 	int a = *ptr_a;
 	int b = *ptr_b;
 	int solution = a + b;
-	int c = *ptr_c;
 	*ptr_c = a + b;
 	printf("C: %i + %i = %i\n", a, b, solution);
 }
