b660000370183444fad5c4d64e8625ea98bc81e0
Brendan Kristiansen <brendankristiansen@icloud.com>
2017-10-04 19:10:16 -0600

    Work on moving data up to Fortran

--- read-mesh/c_wrapper.c
+++ read-mesh/c_wrapper.c
@@ -1,15 +1,39 @@
 #include<stdio.h>
 #include<silo.h>
 
+typedef struct fake_mesh {
+	int dimensions;		//Dimensions in mesh
+	int origin;		//Origin of array
+	int index_size;		//Size of start_index array
+	int start_index[];	//Index of start points
+}fake_mesh;
+
+void test_mesh_(int *dimensions, int *origin, int *index_size, int **start_index){
+	printf("Test mesh!\n");
+	int dims = 3;
+	int orig = 0;
+	int size = 3;
+	//*start_index;
+	*index_size = size;
+	*origin = orig;
+	*dimensions = dims;
+}
+
 void read_mesh_(int *dimensions, int *origin, int *index_size, int **start_index){
 	printf("Reading mesh in C...\n");
 	DBfile *openfile = DBOpen("sample1.silo", DB_HDF5, DB_READ);
-	printf("Putting quadmesh");
+	printf("Putting quadmesh\n");
 	DBquadmesh *readmesh = DBGetQuadmesh(openfile, "testdir/mesh");
-	printf("read dim[0] is %d\n", readmesh->dims[0]);
-	//*dimensions = readmesh->dims;
-	//*origin = readmesh->origin;
-	//*index_size = sizeof(readmesh->start_index);
+	printf("Mesh information: (C):\n");
+	printf("Dimensions:\t%i\n", readmesh->dims);
+	printf("Origin:\t%i\n", readmesh->origin);
+	printf("index_size:\t\i\n", sizeof(readmesh->start_index));
+	for(int i = 0; i < index_size; i++){
+		//printf("Start index %i:\t%i\n", i, readmesh->start_index[i]);
+	}
+	*dimensions = readmesh->dims;
+	*origin = readmesh->origin;
+	*index_size = sizeof(readmesh->start_index);
 	*start_index = readmesh->start_index;
 }
 
--- read-mesh/driver.f90
+++ read-mesh/driver.f90
@@ -4,8 +4,16 @@ program driver
     integer :: iorigin=0
     integer :: iindex=0
     integer, dimension (3) :: istart=0
+    call test_mesh(idims, iorigin, iindex, istart)
+    print *, "Mesh Information!!"
+    print *, "Dimensions:", idims
+    print *, "Origin:", iorigin
+    print *, "Start index for each dimension:"
+    do iorigin = iorigin, iindex, 1
+        print *, istart(iorigin)
+        end do
     call read_mesh(idims, iorigin, iindex, istart)
-    print *, "Mesh information:"
+    print *, "Mesh information (Fortran):"
     print *, "Dimensions: ", idims
     print *, "Origin: ", iorigin
     print *, "Start index for each dimension:"
