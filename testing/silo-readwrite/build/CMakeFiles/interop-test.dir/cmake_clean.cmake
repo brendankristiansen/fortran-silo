db0a805e15fca39e4f0e3997b79b13af2b795ae1
Brendan Kristiansen <brendankristiansen@icloud.com>
2017-11-16 16:32:55 -0700

    Move everything to testing directory

---
+++ testing/silo-readwrite/build/CMakeFiles/interop-test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interop-test.dir/wrapper.c.o"
+  "CMakeFiles/interop-test.dir/silo_readwrite.f90.o"
+  "interop-test.pdb"
+  "interop-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C Fortran)
+  include(CMakeFiles/interop-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
